016f609bb2a885c98132d70af1b9c3eb63f11e42
ddavid-vieira <davidvieira0110@gmail.com>
2025-05-01 20:30:38 -0300

    feat: add region to organize patterns

--- DesignPatternsCsharp/Creational/Builder/Director.cs
+++ DesignPatternsCsharp/Creational/Builder/Director.cs
@@ -1,30 +1,23 @@
 namespace DesignPatternsCsharp.Creational.Builder;
 
 // (Optional) The Director is only responsible for executing the building steps in a particular sequence.
-public class Director
+public class Director(IBuilder builder)
 {
-    private readonly IBuilder _builder;
-
-    public Director(IBuilder builder)
-    {
-        _builder = builder;
-    }
-
     public void ConstructSportsCar()
     {
-        _builder.Reset();
-        _builder.SetSeats(2);
-        _builder.SetEngine("V8");
-        _builder.SetTripComputer(true);
-        _builder.SetGps(true);
+        builder.Reset();
+        builder.SetSeats(2);
+        builder.SetEngine("V8");
+        builder.SetTripComputer(true);
+        builder.SetGps(true);
     }
 
     public void ConstructSuv()
     {
-        _builder.Reset();
-        _builder.SetSeats(4);
-        _builder.SetEngine("V6");
-        _builder.SetTripComputer(true);
-        _builder.SetGps(true);
+        builder.Reset();
+        builder.SetSeats(4);
+        builder.SetEngine("V6");
+        builder.SetTripComputer(true);
+        builder.SetGps(true);
     }
 }
